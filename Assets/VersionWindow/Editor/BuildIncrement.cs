3e3a3a90a61aa4c3d8fa49953af7b3501af6370d
Benedikt Hensen <hensen@dbis.rwth-aachen.de>
2020-01-30 14:31:25 +0100

    integrated build increment

---
+++ Assets/VersionWindow/Editor/BuildIncrement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace i5.Editor.Versioning
+{
+    public class BuildIncrement
+    {
+        [PostProcessBuild(1)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+            Debug.Log("Post Build");
+            if (VersionWindow.VersionInfo == null)
+            {
+                VersionWindow.VersionInfo = VersionInfo.TryLoad();
+            }
+
+            VersionWindow.VersionInfo.IncrementBuildVersion();
+            VersionWindow.VersionInfo.Save();
+        }
+    }
+}
