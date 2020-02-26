d2d75f9b478bc11873f8677b1e99a0197ac0ae1f
Benedikt Hensen <benedikt.hensen@rwth-aachen.de>
2020-02-26 12:55:14 +0100

    added sample label to show version in play mode

---
+++ Assets/VersionLabel.cs
@@ -0,0 +1,16 @@
+﻿using i5.Editor.Versioning;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class VersionLabel : MonoBehaviour
+{
+    public TextMeshPro label;
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        label.text = VersionManager.DataInstance.VersionString;
+    }
+}
